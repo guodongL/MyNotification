f0dfb9f5d0414d6b7f484afaf3f1c32a01e0ffa0
guodongL <some_kool@outlook.com>
2018-09-29 15:54:30 +0800

    第一次提交

--- app/src/main/java/com/example/administrator/mynotification/MainActivity.java
+++ app/src/main/java/com/example/administrator/mynotification/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.administrator.mynotification;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonClick;
+    private Context mContext=this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        buttonClick=findViewById(R.id.buttonClick);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext,"上传到Git上",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
